deba91e52fb66e9b41f99da422c0615f5c446ca3
rhishikeshj <rhishikeshj@gmail.com>
2019-12-31 11:03:16 +0530

    Fix day 3 solution.
    I was missing the first segment starting 0,0 to x1,y1 :D
    Took me a good 3 hours to fix ! Turns out, discussing this with my
    better half was the way to solve it :D

--- src/advent_of_code/2019/day3.clj
+++ src/advent_of_code/2019/day3.clj
@@ -1,6 +1,5 @@
 (ns advent-of-code.2019.day3
   (:require [clojure.string :as str]))
-(require '[clojure.string :as str])
 
 (def step1* (str/split "R1004,U518,R309,D991,R436,D360,L322,U627,R94,D636,L846,D385,R563,U220,L312,D605,L612,D843,R848,U193,L671,D852,L129,D680,L946,D261,L804,D482,R196,U960,L234,U577,R206,D973,R407,D400,R44,D103,R463,U907,L972,U628,L962,U856,L564,D25,L425,U332,R931,U837,R556,U435,R88,U860,L982,D393,R793,D86,R647,D337,R514,D361,L777,U640,R833,D674,L817,D260,R382,U168,R161,U449,L670,U814,L42,U461,R570,U855,L111,U734,L699,U602,R628,D79,L982,D494,L616,D484,R259,U429,L917,D321,R429,U854,R735,D373,L508,D59,L207,D192,L120,D943,R648,U245,L670,D571,L46,D195,L989,U589,L34,D177,L682,U468,L783,D143,L940,U412,R875,D604,R867,D951,L82,U851,L550,D21,L425,D81,L659,D231,R92,D232,R27,D269,L351,D369,R622,U737,R531,U693,R295,U217,R249,U994,R635,U267,L863,U690,L398,U576,R982,U252,L649,U321,L814,U516,R827,U74,L80,U624,L802,D620,L544,U249,R983,U424,R564,D217,R151,U8,L813,D311,R203,U478,R999,U495,R957,U641,R40,U431,L830,U67,L31,U532,R345,U878,L996,D223,L76,D264,R823,U27,L776,U936,L614,U421,L398,U168,L90,U525,R640,U95,L761,U938,R296,D463,L349,D709,R428,U818,L376,D444,L748,D527,L755,U750,R175,U495,R587,D767,L332,U665,L84,D747,L183,D969,R37,D514,R949,U985,R548,U939,L170,U415,R857,D480,R836,D363,R763,D997,R721,D140,R699,U673,L724,U375,R55,U758,R634,D590,L608,U674,R809,U308,L681,D957,R30,D913,L633,D939,L474,D567,R290,D615,L646,D478,L822,D471,L952,D937,R306,U380,R695,U788,R555,D64,R769,D785,R115,U474,R232,U353,R534,D268,L434,U790,L777,D223,L168,U21,L411,D524,R862,D43,L979,U65,R771,U872,L983,U765,R162" #","))
 
@@ -47,8 +46,7 @@
       \R {:x (+ x dist) :y y}
       \L {:x (- x dist) :y y}
       \D {:x x :y (- y dist)}
-      \U {:x x :y (+ y dist)}
-      )))
+      \U {:x x :y (+ y dist)})))
 
 
 (defn get-path
@@ -67,11 +65,12 @@
 
 (defn find-intersections
   [steps1 steps2]
-  (let [first-path (get-path [] steps1)
-        second-path (get-path [] steps2)]
-    (for [segment1 (get-path-segments first-path)
-          segment2 (get-path-segments second-path)]
-      (intersection-point segment1 segment2))))
+  (let [first-path (get-path [{:x 0 :y 0}] steps1)
+        second-path (get-path [{:x 0 :y 0}] steps2)]
+    (filter identity
+            (for [segment1 (get-path-segments first-path)
+                  segment2 (get-path-segments second-path)]
+              (intersection-point segment1 segment2)))))
 
 
 (defn find-distances
@@ -79,7 +78,7 @@
   (map (fn [point]
        (manhattan-distance {:x 0 :y 0}
                            point))
-     (filter identity (find-intersections step1 step2))))
+     (find-intersections step1 step2)))
 
 
 (defn minimum-distance
