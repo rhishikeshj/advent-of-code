e901b4becd0a41e5fcbf10c7526b7673c6784356
Rhishikesh Joshi <rhishikeshj@gmail.com>
2021-12-01 10:40:47 +0100

    elixir: Day1 part 1

---
+++ elixir/advent_of_code/lib/2021/day1.ex
@@ -0,0 +1,33 @@
+defmodule AOC.Day1 do
+  @moduledoc """
+  Solution to Day 1 of the Advent of code 2021
+  https://adventofcode.com/2021/day/1
+  """
+
+  @doc """
+  Read the input file
+  """
+  @spec get_inputs(File) :: [String.t()]
+  def get_inputs(f \\ "lib/inputs/day1.txt"),
+    do: File.read!(f) |> String.trim() |> String.split("\n")
+
+  @doc """
+  Count the number of times a given fun returns true for an element and it's predecessor in a list.
+  fun is a function which gets the current element as the first arg and prev element as the second arg.
+  """
+  @spec count_when_prev(list, fun()) :: pos_integer
+  def count_when_prev(list, fun) do
+    {count, _a} =
+      Enum.reduce(list, {0, Enum.at(list, 0)}, fn x, {c, prev} ->
+        if fun.(x, prev), do: {c + 1, prev}, else: {c, x}
+      end)
+
+    count
+  end
+
+  @doc """
+  Count the number of times a number is greater than its predecessor in a list
+  """
+  @spec count_increases(list) :: pos_integer
+  def count_increases(inputs \\ get_inputs()), do: count_when_prev(inputs, &>/2)
+end
