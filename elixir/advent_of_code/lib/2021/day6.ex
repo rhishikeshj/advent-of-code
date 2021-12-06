52eb16f16de9dbdb41299ecf0bd8bd395fd9439f
Rhishikesh Joshi <rhishikeshj@gmail.com>
2021-12-06 21:42:49 +0100

    elixir: Day6 part 2
    Moving from calculating for 80 days to 256 days really changes the peformance demands from
    the solution, to the point that a completely new strategy is required !
    This solution is akin to a counting sort...

--- elixir/advent_of_code/lib/2021/day6.ex
+++ elixir/advent_of_code/lib/2021/day6.ex
@@ -14,44 +14,44 @@ defmodule AOC.Day6 do
     |> String.trim()
     |> String.split(",")
     |> Enum.map(&String.to_integer/1)
+    |> Enum.frequencies()
+    |> Enum.into(%{0 => 0, 1 => 0, 2 => 0, 3 => 0, 4 => 0, 5 => 0, 6 => 0, 7 => 0, 8 => 0})
   end
 
+  @doc """
+  Increment the internal timer of lanternfish by a day
+  Each day, a 0 becomes a 6 and adds a new 8 to the end of the list,
+  while each other number decreases by 1 if it was present at the start of the day.
+  """
   def inc_day(ages) do
-    ages |> Stream.map(&(&1 - 1))
-  end
-
-  def spawn_new_fish(ages) do
-    new_fish =
-      ages
-      |> Enum.count(&(&1 == -1))
-
     ages
-    |> Stream.map(&if &1 == -1, do: 6, else: &1)
-    |> then(
-      &if new_fish > 0 do
-        Stream.concat(
-          &1,
-          for _i <- 0..(new_fish - 1) do
-            8
-          end
-        )
-      else
-        &1
-      end
-    )
+    |> Enum.map(fn
+      {8, v} -> {8, ages[0]}
+      {6, v} -> {6, ages[7] + ages[0]}
+      {k, v} -> {k, ages[k + 1]}
+    end)
+    |> Enum.into(%{})
   end
 
   def move_a_day(ages, 0), do: ages
 
   def move_a_day(ages, days) do
-    move_a_day(ages |> inc_day |> spawn_new_fish, days - 1)
+    move_a_day(ages |> inc_day, days - 1)
   end
 
   def part1(file \\ "lib/inputs/day6.txt") do
     file
     |> get_inputs
     |> move_a_day(80)
-    |> Enum.to_list()
-    |> length
+    |> Map.values()
+    |> Enum.sum()
+  end
+
+  def part2(file \\ "lib/inputs/day6.txt") do
+    file
+    |> get_inputs
+    |> move_a_day(256)
+    |> Map.values()
+    |> Enum.sum()
   end
 end
